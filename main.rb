b3a0aeaf2a9360e631fbeee9f3ea58da9543a1ea
Utah Kate Newman <utah.kate.newman@gmail.com>
2016-01-05 17:02:22 -0800

    spin up basic app - w some dummy text from prework site

---
+++ main.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+
+get '/' do
+  erb :index
+end
+
+get '/day0' do
+  erb :day0
+end
+
+get '/day1' do
+  erb :day1
+end
+
+get '/day2' do
+  erb :day2
+end
+
+get '/day3' do
+  erb :day3
+end
+
+get '/day4' do
+  erb :day4
+end
